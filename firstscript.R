f348c668f5604cdb9e675e59d5e929429799e192
KChodara <krzy.chodara@gmail.com>
2021-03-20 18:28:48 +0100

    added files added new funtions to firstscript

--- firstscript.R
+++ firstscript.R
@@ -21,3 +21,65 @@ weatherText <- content(getWeather, as="text")
 weatherJSON <- fromJSON(weatherText, flatten=FALSE)
 weatherDF <- as.data.frame(weatherJSON)
 View(weatherDF)
+
+
+x <- 125.6
+x <- "123"
+class(x)
+x<-c(1,2,3,4,5)
+x
+class(x)
+
+x <- as.integer(x)
+class(x)
+y <- 2
+class(x+y)
+
+
+v1<-as.integer(v1)
+v2<-as.vector(c(2,4),mode="integer")
+
+wynik<- v1-v2
+class(wynik)
+wynik<- v1*v2
+class(wynik)
+wynik<- v1+v2
+class(wynik)
+wynik<- v2/v1
+class(wynik)
+
+wynik<- v1%%v2
+wynik<- v1%/%v2
+
+vecto<-(c(v2,TRUE,FALSE, -1, 0))
+vectolo <- as.logical(vecto)
+v1;v2
+
+
+l <- list(1,3, vecto)
+l[1]
+lista <- list(1,2,3,4)
+lista[[1]][lista[[1]]>1]
+
+piec <- c("kocioł", "gaz","kocioł", "gaz")
+piecf <- as.factor(piec)
+str(piecf)
+as.character(piecf)
+
+df<- data.frame(index=1:3, imie=c("jan","alina","bartek"),plec=c(21,12,37), stringsAsFactors = FALSE)
+str(df)
+View(df)
+
+
+df2 <- read.csv('dane.csv', sep=';')
+df2 <- read.table('dane.csv', sep=';', header = TRUE)
+View(df2)
+
+for (i in 1:10){
+  if (i==2){
+    print(i)
+  }
+  else
+  {print(i**2)}
+}
+
